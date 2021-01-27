56a57821359530fc1282f6452f26c4b49d621e06
DJ Huff <dh866218@ohio.edu>
2021-01-26 20:53:43 -0500

    git-lab-preogram

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <DJ Huff>
+ *        Date: <01/26/21>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+ cout << "Hello Git!!" << endl;
+    return 0;
+}// main
